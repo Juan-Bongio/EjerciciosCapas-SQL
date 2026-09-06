4ac22df558d0dff3e51bb55dd949df276fca25c8
Juan M. <jmbonyo08@gmail.com>
2026-09-06 20:17:39 -0300

    Ejercicios de Programacion en Capas, con conexion en SQL

---
+++ SolucionCapas/SolucionCapas.Datos/Datos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SolucionCapas.Datos
+{
+    public class PersonaDatos
+    {
+        private List<(int Dni, string Nombre, string Telefono, string Direccion, string Ciudad)> _tablaPersonas = new List<(int, string, string, string, string)>
+        {
+            (23269431, "Alberto", "1123456789", "Av. Siempre Viva 123", "Buenos Aires"),
+            (27576691, "Noemi", "1167894321", "Calle San Martin 456", "Caseros")
+        };
+
+        public (int Dni, string Nombre, string Telefono, string Direccion, string Ciudad)? BuscarPorDni(int dni)
+        {
+            var resultado = _tablaPersonas.FirstOrDefault(p => p.Dni == dni);
+
+            if (Convert.ToInt32(resultado.Dni) == null)
+                return null;
+
+            return resultado;
+        }
+    }
+}
---
+++ SolucionCapas/SolucionCapas.Negocio/Negocio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SolucionCapas.Datos;
+
+namespace SolucionCapas.Negocio
+{
+    public class Persona
+    {
+        public int Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+    }
+
+    public class PersonaNegocio
+    {
+        private PersonaDatos _datos = new PersonaDatos();
+
+        public Persona ObtenerPersona(int dni)
+        {
+            if (dni <= 0)
+                return null;
+
+            var resultado = _datos.BuscarPorDni(dni);
+
+            if (resultado == null)
+                return null;
+
+            return new Persona
+            {
+                Dni = Convert.ToInt32(resultado.Value.Dni),
+                Nombre = resultado.Value.Nombre,
+                Telefono = resultado.Value.Telefono,
+                Direccion = resultado.Value.Direccion,
+                Ciudad = resultado.Value.Ciudad
+            };
+        }
+    }
+}
